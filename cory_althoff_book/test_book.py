e4f78fa803112617a0e2d97ed4015d64ba094c0a
Prince Aleksey <chirkinaa@pgk.ru>
2023-09-09 22:48:10 +0300

    add homework

---
+++ cory_althoff_book/test_book.py
@@ -0,0 +1,96 @@
+# for i in range(100):
+#     print('Я буду инжинером програмистом')
+
+
+# import math
+#
+# l = 4
+# w = 10
+# d = math.sqrt(l**2 + w**2)
+# print(d)
+
+
+# print((2 * 3) - (2**2))
+
+# home = "Russia"
+# if home == "Russia":
+#     print(f"Привет {home}")
+# else:
+#     print("Привет бомж")
+
+
+# print(10 % 2)
+# print(10 / 2)
+# print(10 // 2)
+
+# x = 2
+# if x == 2:
+#     print(f"Число: {x}")
+# if x % 2 == 0:
+#     print("Число четное")
+# if x % 2 != 0:
+#     print("Число нечетное")
+
+# x = 10
+# y = 11
+# if x == 10:
+#     if y == 12:
+#         print(x + y)
+#     else:
+#         print('ничего не вышло')
+
+# home = 'Тайланд'
+# if home == 'Япония':
+#     print('привет Япония')
+# elif home == 'Тайланд':
+#     print('привет Тайланд')
+# elif home == 'Индия':
+#     print('привет Индия')
+# elif home == 'Китай':
+#     print('Китай')
+# else:
+#     print('Привет мир')
+
+
+# home = 'Марс'
+# if home == 'россия':
+#     print('Привет россия')
+# elif home == 'Казахстан':
+#     print('привет казахстан')
+# elif home == 'Тайнланд':
+#     print('Привет Тайнланд')
+# elif home == 'Мексика':
+#     print('Привет Мексика')
+# else:
+#     print('привет мир')
+
+
+# x = 100
+# if x == 10:
+#     print('10')
+# elif x == 20:
+#     print('20')
+# else:
+#     print('не знаю')
+# if x == 100:
+#     print('x == 100')
+# if x % 2 == 0:
+#     print('x четное')
+# else:
+#     print('x нечетное')
+
+# практикум
+
+# a = 10
+# if a < 10:
+#     print(f'{a} меньше 10')
+# elif a >= 10:
+#     print(f'{a} больше или равно 10')
+
+any_random = 26
+if any_random <= 10:
+    print(f'{any_random} меньше или равно 10')
+elif any_random > 10 and any_random <= 25:
+    print(f'{any_random} больше 10, но меньше или равно 25')
+else:
+    print(f'{any_random} больше 25')
--- homework_while/test.py
+++ homework_while/test.py
@@ -1,1 +1,55 @@
-print(2 + 1)
+# n = 6
+# total = 0
+#
+# while n:
+#     print(f'process n {n}')
+#     total += n
+#     n -= 1
+# print(total)
+
+
+# n = 6
+# total = 0
+#
+# while n:
+#     print(f'process n {n}')
+#     if n < 4:
+#         break
+#     total += n
+#     n -= 1
+# print(total)
+
+
+# number = 123456
+# nums_total = 0
+#
+# while number:
+#     remainder = number % 10
+#     if remainder < 4:
+#         break
+#     nums_total += remainder
+#     number //= 10
+# print(f'nums_total {nums_total}')
+# print(number)
+
+
+# memory = None
+#
+# while True:
+#     command = input('Command: ')
+#     if command == 'r':
+#         print(f'Memory {memory}')
+#     elif command == 'w':
+#         memory = input('Помнить: ')
+#     elif command == 'q':
+#         break
+#     else:
+#         print('знать команды r,w,q')
+
+
+cash = int(input('Сколько мне платят ЗП: '))
+
+if cash >= 230000:
+    print('Делаем красивую работу')
+else:
+    print('Открывается вакансия на HH')
