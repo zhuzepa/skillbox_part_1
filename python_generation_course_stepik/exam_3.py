03655ab98df1087112c7343ec9a692c9854cd8af
Prince Aleksey <chirkinaa@pgk.ru>
2023-11-01 10:14:30 +0300

    create HW_3

---
+++ python_generation_course_stepik/exam_3.py
@@ -0,0 +1,25 @@
+# Итоговая работа на ввод и вывод данных
+
+
+# 1
+# print(17 * "*")
+# print("*", "*", sep=15 * " ")
+# print("*", "*", sep=15 * " ")
+# print(17 * "*")
+
+# 2
+
+# a, b = int(input()), int(input())
+# c = ((a + b) ** 2)
+# d = (a ** 2 + b ** 2)
+# print(f'Квадрат суммы {a} и {b} равен {c}')
+# print(f'Сумма квадратов {a} и {b} равна {d}')
+
+# 3
+
+# a, b, c, d = int(input()), int(input()), int(input()), int(input())
+# print((a ** b) + (c ** d))
+
+# 4
+n = int(input())
+print(n + n * 10 + n + n * 100 + n *10 + n)
